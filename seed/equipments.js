468a1d004b23014572dc5cd9d18907033a026d2f
GideonCove <gideoncove@gmail.com>
2024-05-28 18:07:50 -0400

    added bats to equipment

--- seed/equipments.js
+++ seed/equipments.js
@@ -1,4 +1,4 @@
-// ball,bat,gloves,headGear,padding
+// ball,BAT,gloves,headGear,padding
 const db = require('../db')
 const { Sport, Equipment } = require('../models')
 
@@ -14,15 +14,130 @@ const main = async () => {
   const lacrosse = await Sport.find({ name: "Lacrosse"})
   const tennis = await Sport.find({ name: "Tennis"})
   
-
-
-
   const equipments = [
     {
-        sport_id: fightClub[0]._id,
-        item: { type: String, required: true },
-        price: { type: Number, required: true }
-      },
+        sport_id: hockey[0]._id,
+        item: "CCM Ribcore 3 Hockey Stick",
+        price: 80
+    },
+    {
+      sport_id: hockey[0]._id,
+      item: "Bauer Vapor X2.7 Hockey Stick",
+      price: 120
+    },
+    {
+      sport_id: hockey[0]._id,
+      item: "Warrior Alpha DX Hockey Stick",
+      price: 150
+    },
+    {
+      sport_id: hockey[0]._id,
+      item: "True AX9 Hockey Stick",
+      price: 180
+    },
+    {
+      sport_id: hockey[0]._id,
+      item: "CCM JetSpeed FT3 Pro Hockey Stick",
+      price: 250
+    },
+    {
+      sport_id: hockey[0]._id,
+      item: "Sher-Wood Rekker M90 Hockey Stick",
+      price: 200
+    },
+    {
+      sport_id: tennis[0]._id,
+      item: "Wilson Pro Staff RF97 Autograph Tennis Racket",
+      price: 220
+    },
+    {
+        sport_id: tennis[0]._id,
+        item: "Babolat Pure Aero Tennis Racket",
+        price: 200
+    },
+    {
+        sport_id: tennis[0]._id,
+        item: "Head Graphene 360+ Speed Pro Tennis Racket",
+        price: 250
+    },
+    {
+        sport_id: tennis[0]._id,
+        item: "Yonex EZONE 98 Tennis Racket",
+        price: 230
+    },
+    {
+        sport_id: tennis[0]._id,
+        item: "Prince Textreme Tour 100P Tennis Racket",
+        price: 180
+    },
+    {
+        sport_id: tennis[0]._id,
+        item: "Dunlop CX 200 Tennis Racket",
+        price: 170
+    },
+    {
+      sport_id: baseball[0]._id,
+      item: "Louisville Slugger Omaha 518 BBCOR Baseball Bat",
+      price: 100
+    },
+    {
+        sport_id: baseball[0]._id,
+        item: "Marucci CAT7 BBCOR Baseball Bat",
+        price: 200
+    },
+    {
+        sport_id: baseball[0]._id,
+        item: "Easton Ghost X BBCOR Baseball Bat",
+        price: 300
+    },
+    {
+        sport_id: baseball[0]._id,
+        item: "DeMarini Voodoo One BBCOR Baseball Bat",
+        price: 250
+    },
+    {
+        sport_id: baseball[0]._id,
+        item: "Rawlings Quatro Pro BBCOR Baseball Bat",
+        price: 400
+    },
+    {
+        sport_id: baseball[0]._id,
+        item: "Mizuno MaxCor BBCOR Baseball Bat",
+        price: 150
+    },
+    {
+      sport_id: lacrosse[0]._id,
+      item: "STX Crux 600 Women's Lacrosse Stick",
+      price: 120
+    },
+    {
+        sport_id: lacrosse[0]._id,
+        item: "Epoch Dragonfly Integra X Women's Lacrosse Stick",
+        price: 200
+    },
+    {
+        sport_id: lacrosse[0]._id,
+        item: "Maverik Union Men's Lacrosse Stick",
+        price: 100
+    },
+    {
+        sport_id: lacrosse[0]._id,
+        item: "STX Stallion 700 Men's Lacrosse Stick",
+        price: 150
+    },
+    {
+        sport_id: lacrosse[0]._id,
+        item: "Warrior Burn Pro Diamond Men's Lacrosse Stick",
+        price: 180
+    },
+    {
+        sport_id: lacrosse[0]._id,
+        item: "Brine Clutch Elite Men's Lacrosse Stick",
+        price: 250
+    }
+
+  
+
 ]
 
 await Equipment.insertMany(equipments)
