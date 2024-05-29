15721bd9833530089dadbc1037b645b883620436
Tobias Dodge <tobydodge65@gmail.com>
2024-05-29 13:23:31 -0230

    Added a seedall script

--- controllers/equipmentcontroller.js
+++ controllers/equipmentcontroller.js
@@ -1,12 +1,12 @@
-const {Equipment, Sport, Clothing} = require('../models');
-const { get } = require('../models/clothing');
+const {Equipment, Sport, Clothing} = require('../models')
+const { get } = require('../models/clothing')
 
 const getEquipments = async (req, res) => {
     try {
         const equipments = await Equipment.find()
         res.json(equipments)
     } catch (error) {
-        return res.status(500).send(error.message);
+        return res.status(500).send(error.message)
     }
 }
 
@@ -16,7 +16,7 @@ const getEquipment = async(req, res) => {
         const equipments = await Equipment.findById(id)
         res.json(equipments)
     } catch (error) {
-        return res.status(500).send(error.message);
+        return res.status(500).send(error.message)
     }
 }
 
@@ -26,7 +26,7 @@ const getEquipmentBySportId = async(req, res) => {
         const equipments = await Equipment.find({sport_id: sportId})
         res.json(equipments)
     } catch (error) {
-        return res.status(500).send(error.message);
+        return res.status(500).send(error.message)
     }
 }
 
@@ -36,7 +36,16 @@ const addEquipment = async(req, res) => {
         await equipment.save()
         res.json(equipment)
     } catch (error) {
-        return res.status(500).send(error.message);
+        return res.status(500).send(error.message)
+    }
+}
+
+const updateEquipment = async(req, res) => {
+    try {
+        const {id} = req.params
+        await Equipment.findByIdAndUpdate(id, req.body, {new: true})
+    } catch (error) {
+        return res.status(500).send(error.message)
     }
 }
 
@@ -44,5 +53,6 @@ module.exports = {
     getEquipments,
     getEquipment,
     getEquipmentBySportId,
-    addEquipment
+    addEquipment,
+    updateEquipment
 }
