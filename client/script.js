802ad4491fb17438f1ba558659b3187835ffc9d9
MyCloudAtl <megandmashburn@gmail.com>
2024-05-29 14:27:15 -0400

    added images

--- client/script.js
+++ client/script.js
@@ -15,12 +15,11 @@ let sportImage = document.querySelector("#sportImage")
 let sportItem = document.querySelector("#sportItem")
 let sportDisplay = document.querySelector("#imageArray")
 
-button.addEventListener('click', async (e)=> {
-    e.preventDefault()
+button.addEventListener('click', async ()=> {
     sportName.innerHTML = ``
     let input = document.querySelector("#inputBar").value
     if (input!== ``) {
         let response = await axios.get(
-            'http://localhost:3000/${input}'
+            `http://localhost:3000/${input}`
         )
         console.log(response.data)}})
