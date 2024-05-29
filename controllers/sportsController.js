53a17d1550496c4c0bec2957305a9338c36a619c
Tobias Dodge <tobydodge65@gmail.com>
2024-05-29 11:22:48 -0230

    Added sport controller

---
+++ controllers/sportsController.js
@@ -0,0 +1,28 @@
+const { Sport } = require('../models')
+
+const getSportItems = async (req, res) => {
+    try {
+        const items = await Models.Sport.find()
+        return res.status(200).json({ items })
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getSportItem = async (req, res) => {
+    try {
+        const { id } = req.params
+        const item = await Models.Sport.findById(id)
+        if (item) {
+            return res.status(200).json({ item })
+        }
+        return res.status(404).send('Item with the specified ID does not exist')
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+module.exports = {
+    getSportItems,
+    getSportItem
+}
