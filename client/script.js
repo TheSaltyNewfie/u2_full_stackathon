5d04a2f7cc21f4c94957d4b10e88f448f55b4334
Tobias Dodge <tobydodge65@gmail.com>
2024-05-29 23:33:30 -0230

    Added basic code for frontend

--- client/script.js
+++ client/script.js
@@ -1,11 +1,26 @@
-const datadiv = document.getElementById('api-results');
+const sportsEndpoint = 'http://127.0.0.1:3000/sports'
+const clothesEndpoint = 'http://localhost:3000/clothes'
+const equipmentEndpoint = 'http://localhost:3000/equipment'
 
-async function getData() {
-    const response = await fetch('http://localhost:3000/');
-    const data = await response.text();
-    console.log(data);
-    datadiv.innerHTML = data;
+const dropdown = document.getElementById('dropdown')
+const clothingDiv = document.getElementById('content1')
+const equipmentDiv = document.getElementById('content2')
+const shoppingCart = document.getElementById('sidebar')
+
+async function getItems(endpoint) {
+    try {
+        const resp = await axios.get(`${endpoint}`)
+        console.log(resp.data)
+        resp.data.clothes.forEach(item => {
+            clothingDiv.innerHTML += `
+                <p>${item.name}</p>
+            `
+        })
+    } catch(error) {
+        console.error(error)
+    }
 }
 
-getData();
-
+dropdown.onchange = async (e) => {
+    await getItems(`${sportsEndpoint}/id/${dropdown.options[dropdown.selectedIndex].text.toLowerCase()}`)
+}
